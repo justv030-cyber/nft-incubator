420c2a3eb7863ca09604038622fa14a4d56418f6
justv030-cyber <justv030@gmail.com>
2026-09-09 19:33:54 +0530

    ADD STAKE FUNC AND UNSTAKE FUN AND ADD  CALCULATE REWARD

---
+++ staking.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract staking {
+    IERC721 immutable NFT;
+    IERC20 immutable Token;
+
+    mapping(address => mapping(uint256 => uint256)) public stakes;
+
+    constructor(address _NFT, address _Token) {
+        NFT = IERC721(_NFT);
+        Token = IERC20(_Token);
+    }
+
+    function calculateRate(uint256 _time)public pure returns (uint256 time) {
+        if (_time < 1 minutes) {
+            return 0;
+        }else if (_time < 3 minutes){
+            return 3;
+        }
+        else if(_time < 4 minutes){
+            return 4;
+        }
+        else{
+            return 10;
+        }
+    }
+
+    function stake(uint256 _tokenId) public {
+        require(
+            NFT.ownerOf(_tokenId) == msg.sender,
+            "You Are Not Owner Of This NFT!"
+        );
+        stakes[msg.sender][_tokenId] = block.timestamp;
+        NFT.transferFrom(msg.sender, address(this), _tokenId);
+    }
+
+    function calculateReward(uint256 _tokenId) public {
+        require(stakes[msg.sender][_tokenId] > 0, "You Dont Have NFT");
+        uint256 time = block.timestamp - stakes[msg.sender][_tokenId];
+    }
+
+    function unstake(uint256 _tokenId) public {}
+}
